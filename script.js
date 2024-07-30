9840fc85ea973b2015962fc707a88320516b42ac
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-07-30 12:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+
+
+let parent=document.querySelector("#parent");
+let count=0;
+parent.id=`image-${count++}`;
+
+parent.addEventListener("dragstart",(e)=>{
+	parent.style.opactity=0.5;
+	e.transferData.setData("image",image.id);
+})
+parent.addEventListener("dragend",()=>{
+	parent.style.opactity=1;
+})
+let arr=["dragover","dragenter","drop"];
+
+for (let drag of arr) {
+	let image=document.querySelectorAll(".image");
+	for (let img of image) {
+		img.addEventListener(drag,(e)=>{
+			 e.preventDefault();
+			if(drag == "drop"){
+               let cardId = e.dataTransfer.getData('image');
+               let taretCard = document.getElementById(cardId);
+               img.append(taretCard);
+           }
+		})
+	}
+	
+}
+
+
